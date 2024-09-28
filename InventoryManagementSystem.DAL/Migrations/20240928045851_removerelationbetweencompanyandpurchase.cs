0dfde433f1d4ad1aa31ae911ba0392cff7b82fd1
MohamedMG7 <mahamedabd096@gmail.com>
2024-09-28 08:00:07 +0300

    add migration after removing relation between compant and purchase

---
+++ InventoryManagementSystem.DAL/Migrations/20240928045851_removerelationbetweencompanyandpurchase.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagementSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class removerelationbetweencompanyandpurchase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Purchases_Company_CompanyId",
+                table: "Purchases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Purchases_CompanyId",
+                table: "Purchases");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "Purchases");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CompanyId",
+                table: "Purchases",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchases_CompanyId",
+                table: "Purchases",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Purchases_Company_CompanyId",
+                table: "Purchases",
+                column: "CompanyId",
+                principalTable: "Company",
+                principalColumn: "CompanyId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
